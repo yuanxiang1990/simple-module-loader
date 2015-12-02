6ebabbacbbe733cc1f0681c23d9edd89ed9373d6
yuanxiang1990 <513757206@qq.com>
2015-12-02 16:40:08 +0800

    修复重复执行的bug

--- module.js
+++ module.js
@@ -2,26 +2,31 @@
  * 简单JS模块加载器
  * 作者 : markbone
  */
-Module.config = {}//模块配置
+Module.config = {} //模块配置
 Module.cache = { //模块缓存
 }
-var STATUS = { 
-　　FETCHING: 1, // The module file is fetching now. 模块正在下载中 
-　　FETCHED: 2, // The module file has been fetched. 模块已下载 
-　　SAVED: 3, // The module info has been saved. 模块信息已保存 
-　　READY: 4, // All dependencies and self are ready to compile. 模块的依赖项都已下载，等待编译 
+var callbackList = {} //回调列
+var STATUS = {　　
+    FETCHING: 1, // The module file is fetching now. 模块正在下载中 
+    　　FETCHED: 2, // The module file has been fetched. 模块已下载 
+    　　SAVED: 3, // The module info has been saved. 模块信息已保存 
+    　　READY: 4, // All dependencies and self are ready to compile. 模块的依赖项都已下载，等待编译 
     EXECUTING: 5, // The module is in compiling now. 模块正在执行
     EXECUTED: 6 // The module is compiled and module.exports is available. 模块已执行 
 }
+
 function Module(id) {
-    this.id = id;//模块ID
-    this.deps = [];//模块依赖
-    this.factory = null;//模块方法
-    this.exports = {};//模块返回接口
-    this.status = STATUS.FETCHED;//模块当前状态,初始化时模块已下载
+    this.id = id; //模块ID
+    this.deps = []; //模块依赖
+    this.factory = null; //模块方法
+    this.exports = {}; //模块返回接口
+    this.status = STATUS.FETCHED; //模块当前状态,初始化时模块已下载
 }
 Module.regr_ready = /(loaded|complete|undefined)/i;
 Module.define = function(id, deps, fn) {
+    if (Module["cache"][id] && Module["cache"][id].status >= STATUS.SAVED) {
+        return;
+    }
     var module = Module["cache"][id] || new Module(id);
     if (typeof deps == "string") {
         deps = Array.call([], deps);
@@ -30,91 +35,85 @@ Module.define = function(id, deps, fn) {
     }
     module.factory = fn;
     module.deps = deps;
-    if(deps.length==0){//没有依赖，直接将模板置为ready状态
+    if (deps.length == 0) { //没有依赖，直接将模板置为ready状态
         module.status = STATUS.READY;
-    }
-    else{
-        module.status = STATUS.SAVED;//模块信息已保存
+    } else {
+        module.status = STATUS.SAVED; //模块信息已保存
     }
     Module.cache[id] = module;
     for (var i = 0; i < module.deps.length; i++) {
         var m = new Module(module.deps[i]);
-        m.status = STATUS.FETCHING;//置模块状态为未下载状态
+        m.status = STATUS.FETCHING; //置模块状态为正在下载中状态
         Module.cache[module.deps[i]] = m;
     };
 }
-Module.use = function(id,callback) { //模块加载入口方法
+Module.use = function(id, callback) { //模块加载入口方法
     Module.load(id);
 }
 Module.load = function(id) {
-    Module.loadJs(id, function() {
-        if (Module.cache[id].deps.length > 0) {
-            for (var i = 0; i < Module.cache[id].deps.length; i++) {
-                var depId = Module.cache[id].deps[i];
-                if(Module.cache[depId].status === STATUS.FETCHING){
+        Module.loadJs(id, function() {
+            if (Module.cache[id].deps.length > 0) {
+                for (var i = 0; i < Module.cache[id].deps.length; i++) {
+                    var depId = Module.cache[id].deps[i];
                     Module.load(depId);
                 }
             }
-        }
-        if (Module.check()) { //全部模块load完成
-            Module.fire();            
-        }
-    });
-}
-//加载依赖的模块
-Module.require = function(id){
-    for(var module in Module.cache){
-        if(Module.cache.hasOwnProperty(module)&&id === Module.cache[module].id){
+            if (Module.check()) { //全部模块load完成
+                Module.fire();
+            }
+        });
+    }
+    //加载依赖的模块
+Module.require = function(id) {
+    for (var module in Module.cache) {
+        if (Module.cache.hasOwnProperty(module) && id === Module.cache[module].id) {
             return Module.cache[module].exports;
         }
     }
 }
-Module.check = function(){
-    for(var module in Module.cache){
-        if(Module.cache.hasOwnProperty(module)&&Module.cache[module].status < STATUS.SAVED){
-            return false;
+Module.check = function() {
+        for (var module in Module.cache) {
+            if (Module.cache.hasOwnProperty(module) && Module.cache[module].status < STATUS.SAVED) {
+                return false;
+            }
         }
+        return true;
     }
-    return true;
-}
-//按依赖执行factory
+    //按依赖执行factory
 Module.fire = function() {
-    var modules = {};
-    Module.copy(modules, Module.cache)
-    while (Module.size(modules) > 0) {
-        for (var c in modules) {
-            if (modules.hasOwnProperty(c)) {
-                if (modules[c].status === STATUS.READY) {
-                    Module.execute(modules[c]);
-                    delete modules[c];
-                }
-                else if(modules[c].status === STATUS.EXECUTED){//已执行过的不再执行
-                    delete modules[c];    
-                } 
-                else {
-                    var canFire = true;
-                    for (var i = 0; i < modules[c].deps.length; i++) {
-                        if (Module.cache[modules[c].deps[i]].status<STATUS.EXECUTED) {//依赖模块未执行过，不能执行
-                            canFire = false;
-                            break;
-                        }
-                    }
-                    if (canFire) {
+        var modules = {};
+        Module.copy(modules, Module.cache)
+        while (Module.size(modules) > 0) {
+            for (var c in modules) {
+                if (modules.hasOwnProperty(c)) {
+                    if (modules[c].status === STATUS.READY) {
                         Module.execute(modules[c]);
                         delete modules[c];
+                    } else if (modules[c].status > STATUS.READY) { //已执行过的或则正在执行中的不再执行
+                        delete modules[c];
+                    } else {
+                        var canFire = true;
+                        for (var i = 0; i < modules[c].deps.length; i++) {
+                            if (Module.cache[modules[c].deps[i]].status < STATUS.EXECUTED) { //依赖模块未执行过，不能执行
+                                canFire = false;
+                                break;
+                            }
+                        }
+                        if (canFire) {
+                            Module.execute(modules[c]);
+                            delete modules[c];
+                        }
                     }
                 }
             }
         }
     }
-}
-
-/**
-*执行模块factory函数
-**/
-Module.execute = function(module){
+    /**
+     *执行模块factory函数
+     **/
+Module.execute = function(module) {
     Module.cache[module.id].status = STATUS.EXECUTING;
-    module.factory(Module.require,module.exports);
+    module.factory(Module.require, module.exports);
     Module.cache[module.id].status = STATUS.EXECUTED;
 }
 Module.copy = function(target, copy) {
@@ -138,10 +137,9 @@ Module.loadJs = function(id, callback) {
     script.type = "text/javascript";
     script.defer = true;
     script.src = Module.config.baseURI + id + ".js";
-    
     if (script.readyState) {
-        script.onreadystatechange = function(){
-            if(Module.regr_ready.test(this.readyState)){
+        script.onreadystatechange = function() {
+            if (Module.regr_ready.test(this.readyState)) {
                 callback && callback();
             }
         }
